669663c6cd6d3216cc77e050917bfe317fd6313b
miguel raz <miguelraz@gmail.com>
2021-04-18 12:33:41 -0500

    spanish translation of 13_extras.jl

--- literate_notebooks/src-ES/13_extras.jl
+++ literate_notebooks/src-ES/13_extras.jl
@@ -1,56 +1,59 @@
 # # Introduction to DataFrames
+# # Introducción a DataFrames
 # **[Bogumił Kamiński](http://bogumilkaminski.pl/about/), May 13, 2018**
+# (Traducción de Miguel Raz Guzmán Macedo, 18 de Abril de 2021)
 
 using DataFrames
 
-# ## Extras - selected functionalities of selected packages
+# ## Extras - funcionalidades especiales de paqueterías selectas
 
 #-
 
-# ### FreqTables: creating cross tabulations
+# ### FreqTables: crear tabulaciones cruzadas
 
 using FreqTables
 df = DataFrame(a=rand('a':'d', 1000), b=rand(["x", "y", "z"], 1000))
-ft = freqtable(df, :a, :b) # observe that dimensions are sorted if possible
+ft = freqtable(df, :a, :b) # nota: las dimensiones se ordenan si es posible
 
 #-
 
-ft[1,1], ft['b', "z"] # you can index the result using numbers or names
+ft[1,1], ft['b', "z"] # puedes indexar el resultado con números o nombres
 
 #-
 
-prop(ft, 1) # getting proportions - 1 means we want to calculate them in rows (first dimension)
+prop(ft, 1) # obtener tamaños - 1 significa que lo queremos calcular en filas (primera dimensión)
+
 
 #-
 
-prop(ft, 2) # and columns are normalized to 1.0 now
+prop(ft, 2) # y las columnas se normalizan a 1.0
 
 #-
 
 x = categorical(rand(1:3, 10))
-levels!(x, [3, 1, 2, 4]) # reordering levels and adding an extra level
-freqtable(x) # order is preserved and not-used level is shown
+levels!(x, [3, 1, 2, 4]) # reorganizar niveles y añadir un nivel extra
+freqtable(x) # el orden se manteine y los nivelos no usados se meustran
 
 #-
 
-freqtable([1,1,2,3,missing]) # by default missings are listed
+freqtable([1,1,2,3,missing]) # por default los valores faltatnes (`missing`s) se muestran en lista
 
 #-
 
-freqtable([1,1,2,3,missing], skipmissing=true) # but we can skip them
+freqtable([1,1,2,3,missing], skipmissing=true) # pero nos los podemos saltar
 
-# ### DataFramesMeta - working on `DataFrame`
+# ### DataFramesMeta - trabajando en un `DataFrame`
 
 using DataFramesMeta
 df = DataFrame(x=1:8, y='a':'h', z=repeat([true,false], outer=4))
 
 #-
 
-@with(df, :x+:z) # expressions with columns of DataFrame
+@with(df, :x+:z) # expresiones con columnas de DataFrame
 
 #-
 
-@with df begin # you can define code blocks
+@with df begin # puedes definir bloques de código
     a = :x[:z]
     b = :x[.!:z]
     :y + [a; b]
@@ -58,41 +61,41 @@ end
 
 #-
 
-a # @with creates hard scope so variables do not leak out
+a # `@with` crea un  `hard scope` (alcance léxico fuerte) y así las variables no se derraman al ambiente
 
 #-
 
 df2 = DataFrame(a = [:a, :b, :c])
-@with(df2, :a .== ^(:a)) # sometimes we want to work on raw Symbol, ^() escapes it
+@with(df2, :a .== ^(:a)) # A veces queremos un `Symbol` a secas, usamos ^() para escapar esa secuencia
 
 #-
 
 df2 = DataFrame(x=1:3, y=4:6, z=7:9)
-@with(df2, _I_(2:3)) # _I_(expression) is translated to df2[expression]
+@with(df2, _I_(2:3)) #   `_I_(expresión)` se traduce a `df2[expression]`
 
 #-
 
-@where(df, :x .< 4, :z .== true) # very useful macro for filtering
+@where(df, :x .< 4, :z .== true) # macro muy útil para filtrar
 
 #-
 
-@select(df, :x, y = 2*:x, z=:y) # create a new DataFrame based on the old one
+@select(df, :x, y = 2*:x, z=:y) # crea un nuevo DataFrame basado en el anterior
 
 #-
 
-@transform(df, a=1, x = 2*:x, y=:x) # create a new DataFrame adding columns based on the old one
+@transform(df, a=1, x = 2*:x, y=:x) # crea un DataFrame agregando columnas basado en el anterior
 
 #-
 
-@transform(df, a=1, b=:a) # old DataFrame is used and :a is not present there
+@transform(df, a=1, b=:a) # se usa el DataFrame anterior y `:a` no está presente ahí
 
 #-
 
-@orderby(df, :z, -:x) # sorting into a new data frame, less powerful than sort, but lightweight
+@orderby(df, :z, -:x) # ordenando los datos en un nuevo DataFrame, menos poderoso que `sort` pero mucho más ligero
 
 #-
 
-@linq df |> # chaining of operations on DataFrame
+@linq df |> # podemos encadenar operaciones sobre un DataFrame
     where(:x .< 5) |>
     orderby(:z) |>
     transform(x²=:x.^2) |>
@@ -100,76 +103,77 @@ df2 = DataFrame(x=1:3, y=4:6, z=7:9)
 
 #-
 
-f(df, col) = df[col] # you can define your own functions and put them in the chain
+f(df, col) = df[col] # puees definir tus propias funcioens y ponerlas en una cadena (`chain` = cadena, como las de metal)
 @linq df |> where(:x .<= 4) |> f(:x)
 
-# ### DataFramesMeta - working on grouped `DataFrame`
+# ### DataFramesMeta - trabajando con `DataFrame` agrupado
 
 df = DataFrame(a = 1:12, b = repeat('a':'d', outer=3))
 g = groupby(df, :b)
 
 #-
 
-@by(df, :b, first=first(:a), last=last(:a), mean=mean(:a)) # more convinient than by from DataFrames
+@by(df, :b, first=first(:a), last=last(:a), mean=mean(:a)) # más conveniente que `by` de `DataFrames.jl`
 
 #-
 
-@based_on(g, first=first(:a), last=last(:a), mean=mean(:a)) # the same as by but on grouped DataFrame
+@based_on(g, first=first(:a), last=last(:a), mean=mean(:a)) #lo mismo que `by` pero en un DataFrame agrupado
 
 #-
 
-@where(g, mean(:a) > 6.5) # filter gropus on aggregate conditions
+@where(g, mean(:a) > 6.5) # filtramos grupos con condiciones agregadas
 
 #-
 
-@orderby(g, -sum(:a)) # order groups on aggregate conditions
+@orderby(g, -sum(:a)) # ordenar grupos con condiciones agregadas
 
 #-
 
-@transform(g, center = mean(:a), centered = :a - mean(:a)) # perform operations within a group and return ungroped DataFrame
+@transform(g, center = mean(:a), centered = :a - mean(:a)) # aplicar operaciones dentro de un grupo y regresar un DataFrame no agrupado
 
 #-
 
-DataFrame(g) # a nice convinience function not defined in DataFrames
+DataFrame(g) # una función auxiliar bonita no definida en DataFrames.jl
 
 #-
 
-@transform(g) # actually this is the same
+@transform(g) # de hecho esto es lo mismo
 
 #-
 
-@linq df |> groupby(:b) |> where(mean(:a) > 6.5) |> DataFrame # you can do chaining on grouped DataFrames as well
+@linq df |> groupby(:b) |> where(mean(:a) > 6.5) |> DataFrame # puedes encadenar operaciones sobre DataFrames agrupado también
 
-# ### DataFramesMeta - rowwise operations on `DataFrame`
+# ### DataFramesMeta - operaciones por filas en un `DataFrame`
 
 df = DataFrame(a = 1:12, b = repeat(1:4, outer=3))
 
 #-
 
-## such conditions are often needed but are complex to write
+## dichas condiciones suelen ser necesarios pero son demasiado complejas para escribirlas
 @transform(df, x = ifelse.((:a .> 6) .& (:b .== 4), "yes", "no"))
 
 #-
 
-## one option is to use a function that works on a single observation and broadcast it
+## una opciön es usar una función que se aplica sobre una sola observación y la broadcasteamos
+# Broadcasting en el manual: https://docs.julialang.org/en/v1/manual/arrays/#Broadcasting 
 myfun(a, b) = a > 6 && b == 4 ? "yes" : "no"
 @transform(df, x = myfun.(:a, :b))
 
 #-
 
-## or you can use @byrow! macro that allows you to process DataFrame rowwise
+## o puedes usar el macro `@byrow`  que permite procesar el DataFrame por filas
 @byrow! df begin
     @newcol x::Vector{String}
     :x = :a > 6 && :b == 4 ? "yes" : "no"
 end
 
-# ### Visualizing data with StatPlots
+# ### Visualizando datos con StatPlots
 
-using StatPlots # you might need to setup Plots package and some plotting backend first
+using StatPlots # Puede ser necesario instalar algunos paquetes de Plots.jl y el backend antes de proseguir
 
 #-
 
-## we present only a minimal functionality of the package
+## presentamos la funcionalidad mínima de este paquete, no nos da para cubrir todo lo que ofrece
 
 #-
 
@@ -178,15 +182,15 @@ df = DataFrame(x = sort(randn(1000)), y=randn(1000), z = [fill("b", 500); fill("
 
 #-
 
-@df df plot(:x, :y, legend=:topleft, label="y(x)") # a most basic plot
+@df df plot(:x, :y, legend=:topleft, label="y(x)") # la gráfica más básica
 
 #-
 
-@df df density(:x, label="") # density plot
+@df df density(:x, label="") # gráfica de densidad
 
 #-
 
-@df df histogram(:y, label="y") # and a histogram
+@df df histogram(:y, label="y") # un histograma
 
 #-
 
