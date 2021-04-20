99665c67eb0a0fb0b849f04f415b647ecd4937f1
Jose Eduardo Storopoli <thestoropoli@gmail.com>
2021-04-20 12:39:00 -0300

    translated 11_performance.jl

--- literate_notebooks/src-PT-BR/11_performance.jl
+++ literate_notebooks/src-PT-BR/11_performance.jl
@@ -1,81 +1,83 @@
-# # Introduction to DataFrames
-# **[Bogumił Kamiński](http://bogumilkaminski.pl/about/), Apr 21, 2018**
+# # Introdução ao DataFrames
+# **[Bogumił Kamiński](http://bogumilkaminski.pl/about/), 23 de Maio de 2018**
+#
+# Tradução de [Jose Storopoli](https://storopoli.io).
 
 using DataFrames
 using BenchmarkTools
 
-# ## Performance tips
+# ## Dicas de desempenho
 
-#-
+# -
 
-# ### Access by column number is faster than by name
+# ### O acesso por número de coluna é mais rápido do que por nome
 
 x = DataFrame(rand(5, 1000))
 @btime x[500];
 @btime x[:x500];
 
-# ### When working with data `DataFrame` use barrier functions or type annotation
+# ### Ao trabalhar com dados `DataFrame`, use funções de barreira ou anotação de tipo
 
-function f_bad() # this function will be slow
-    srand(1); x = DataFrame(rand(1000000,2))
+function f_bad() # essa função será lenta
+    srand(1); x = DataFrame(rand(1000000, 2))
     y, z = x[1], x[2]
     p = 0.0
     for i in 1:nrow(x)
-        p += y[i]*z[i]
+        p += y[i] * z[i]
     end
     p
 end
 
 @btime f_bad();
 
-#-
+# -
 
-@code_warntype f_bad() # the reason is that Julia does not know the types of columns in `DataFrame`
+@code_warntype f_bad() # a razão é que Julia não conhece os tipos de colunas no `DataFrame`
 
-#-
+# -
 
-## solution 1 is to use barrier function (it should be possible to use it in almost any code)
-function f_inner(y,z)
-   p = 0.0
-   for i in 1:length(y)
-       p += y[i]*z[i]
-   end
-   p
+## a solução 1 é usar a função de barreira (deve ser possível usá-la em quase qualquer código)
+function f_inner(y, z)
+    p = 0.0
+    for i in 1:length(y)
+        p += y[i] * z[i]
+    end
+    p
 end
 
-function f_barrier() # extract the work to an inner function
-    srand(1); x = DataFrame(rand(1000000,2))
+function f_barrier() # extrair o trabalho para uma função interna
+    srand(1); x = DataFrame(rand(1000000, 2))
     f_inner(x[1], x[2])
 end
 
-function f_inbuilt() # or use inbuilt function if possible
-    srand(1); x = DataFrame(rand(1000000,2))
+function f_inbuilt() # ou use a função embutida, se possível
+    srand(1); x = DataFrame(rand(1000000, 2))
     dot(x[1], x[2])
 end
 
 @btime f_barrier();
 @btime f_inbuilt();
 
-#-
+# -
 
-## solution 2 is to provide the types of extracted columns
-## it is simpler but there are cases in which you will not know these types
+## a solução 2 é fornecer os tipos de colunas extraídas
+## é mais simples, mas há casos em que você não conhecerá esses tipos
 function f_typed()
-    srand(1); x = DataFrame(rand(1000000,2))
+    srand(1); x = DataFrame(rand(1000000, 2))
     y::Vector{Float64}, z::Vector{Float64} = x[1], x[2]
     p = 0.0
     for i in 1:nrow(x)
-        p += y[i]*z[i]
+        p += y[i] * z[i]
     end
     p
 end
 
 @btime f_typed();
 
-# ### Consider using delayed `DataFrame` creation technique
+# ### Considere usar uma técnica de criação de `DataFrame` "atrasada"
 
 function f1()
-    x = DataFrame(Float64, 10^4, 100) # we work with DataFrame directly
+    x = DataFrame(Float64, 10^4, 100) # trabalhamos com DataFrame diretamente
     for c in 1:ncol(x)
         d = x[c]
         for r in 1:nrow(x)
@@ -94,29 +96,29 @@ function f2()
         end
         x[c] = d
     end
-    DataFrame(x) # we delay creation of DataFrame after we have our job done
+    DataFrame(x) # atrasamos a criação do DataFrame somente depois de terminarmos nosso trabalho
 end
 
 @btime f1();
 @btime f2();
 
-# ### You can add rows to a `DataFrame` in place and it is fast
+# ### Você pode adicionar linhas a um `DataFrame` de maneira *in-place* e é rápido
 
 x = DataFrame(rand(10^6, 5))
 y = DataFrame(transpose(1.0:5.0))
 z = [1.0:5.0;]
 
-@btime vcat($x, $y); # creates a new DataFrame - slow
-@btime append!($x, $y); # in place - fast
+@btime vcat($x, $y); # cria um novo DataFrame - lento
+@btime append!($x, $y); # *in-place* - rápido
 
-x = DataFrame(rand(10^6, 5)) # reset to the same starting point
-@btime push!($x, $z); # add a single row in place - fastest
+x = DataFrame(rand(10^6, 5)) # redefinir para o mesmo ponto de partida
+@btime push!($x, $z); # adiciona uma única linha *in-place* - o mais rápido
 
-# ### Allowing `missing` as well as `categorical` slows down computations
+# ### Permitindo `missing`s assim como `categorical`s reduz o desempenho
 
 using StatsBase
 
-function test(data) # uses countmap function to test performance
+function test(data) # usa a função countmap para testar desempenho
     println(eltype(data))
     x = rand(data, 10^6)
     y = categorical(x)
@@ -131,5 +133,3 @@ test(1:10)
 test([randstring() for i in 1:10])
 test(allowmissing(1:10))
 test(allowmissing([randstring() for i in 1:10]))
-
-
